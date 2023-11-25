d6ef42ce19d5d1fa1aa628114e394ef830de1afc
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-25 11:08:32 -0800

    Added AI Integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-function generatePoem(event) {
-  let poemElement = document.querySelector("#poem");
+function generatePoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    delay: 10,
+    autoStart: true,
+    cursor: null,
+  });
+}
+
+function handleSubmit(event) {
   event.preventDefault();
-  poemElement.innerHTML = "Hello I am a Poem";
+  let userInput = document.querySelector("#subject").value;
+  let prompt = `Generate a poem about ${userInput}`;
+  let context =
+    "You are a poet specializing in iambic pentameter. Follow user instructions. Sign poem at the bottom with 'SheCodes AI";
+  let apiKey = "ab845e08702fbc3c99tc4fo5b8bf92c3";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiURL).then(generatePoem);
 }
 
 let submit = document.querySelector("#submit");
 let subject = document.querySelector("#subject");
 let poemForm = document.querySelector("#poem-form");
 
-poemForm.addEventListener("submit", generatePoem);
+poemForm.addEventListener("submit", handleSubmit);
