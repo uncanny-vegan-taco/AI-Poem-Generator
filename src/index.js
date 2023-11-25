0cad7fd035e5a1a5566c10b48a65d2ebd0be3f14
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-25 10:54:32 -0800

    Added basic JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("Hello There");
+function generatePoem(event) {
+  let poemElement = document.querySelector("#poem");
+  event.preventDefault();
+  poemElement.innerHTML = "Hello I am a Poem";
+}
+
+let submit = document.querySelector("#submit");
+let subject = document.querySelector("#subject");
+let poemForm = document.querySelector("#poem-form");
+
+poemForm.addEventListener("submit", generatePoem);
